477a737b619d3ef2ae2623fe1c9d30ff21dba75b
Dhruv Jain <dhruvjainpenny@gmail.com>
2026-08-21 17:59:33 +0530

    Revert "refactor(storage): move record persistence behind storage"
    This reverts commit 40eecbdf7c9ab5d9055898d100babef773a07a09.

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -10,7 +10,6 @@ export * from "./pg/pagination";
 export * from "./pg/transaction-helpers";
 export * from "./pg/vector-search";
 export * from "./ports";
-export * from "./resources/resource-repo";
 export * from "./runs";
 export * from "./soul";
 export * from "./system";
